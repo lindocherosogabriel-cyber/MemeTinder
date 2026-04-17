d25db6e847665dbec1f7a8e4f2455fdbd6c5fd85
lindocherosogabriel-cyber <lindocherosogabriel@gmail.com>
2026-04-17 15:24:59 -0300

    DB fixed Error again

--- backend/API/dist/connect/connectDB.js
+++ backend/API/dist/connect/connectDB.js
@@ -1,11 +1,14 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+let MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/memeDB";
 export default async function connectDB() {
     try {
-        await mongoose.connect("mongodb://localhost:27017/memeDB");
+        await mongoose.connect(MONGO_URI);
         console.log("Conecção estabelecida com sucesso!");
     }
     catch (error) {
-        throw new Error("não foi possivel se conectar ao bacno de dados");
+        throw new Error("não foi possivel se conectar ao Banco de Dados");
     }
 }
 //# sourceMappingURL=connectDB.js.map
